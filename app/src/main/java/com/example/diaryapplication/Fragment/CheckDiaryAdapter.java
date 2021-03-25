d62a4ea98f9ea51a04dcde79fd7a27ef1e02974b
JinU <sprout0719@naver.com>
2021-03-25 21:02:55 +0900

    add recyclerviewAdapter

---
+++ app/src/main/java/com/example/diaryapplication/Fragment/CheckDiaryAdapter.java
@@ -0,0 +1,31 @@
+package com.example.diaryapplication.Fragment;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.diaryapplication.R;
+
+public class CheckDiaryAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.diary_item, parent, false);
+        return null; //ViewHolder제작해야 함
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
